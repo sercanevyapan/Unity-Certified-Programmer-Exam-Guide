49aac0f8c88240b58f81aabdf6a5aa41feea5f9e
Sercan Evyapan <55910627+sercanevyapan@users.noreply.github.com>
2022-08-12 19:37:45 +0300

    Singleton Pattern

---
+++ Singleton/Assets/Singleton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    private static Singleton _instance;
+
+    public static Singleton Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}
